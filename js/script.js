2148e573650a76d849f930d75448a7645ebb49f3
tarosjk <tarosjk@gmail.com>
2024-07-26 12:03:16 +0900

    MVアニメーション実装（途中）

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ const navToggle = document.querySelector('.header__nav-toggle');
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('open');
 })
+
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
+})
